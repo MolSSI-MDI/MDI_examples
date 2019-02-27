806da82bced2aed558ac1a3d074b9d55634c15b8
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-02-27 14:08:39 -0500

    Add CXX MD test

---
+++ tests/md/run.sh
@@ -0,0 +1,17 @@
+#location of required codes
+LAMMPS_LOC=$(cat ../locations/lammps)
+
+#remove old files
+./clean.sh
+
+#launch LAMMPS
+cd mm
+${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" -in water.in > input.out &
+cd ../
+
+#launch driver
+#cd ../../MDI_examples/; python md.py -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+
+../../build/md -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+
+wait
